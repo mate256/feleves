2cc67a6bc30d7efca609d7b7c097f9fc7682effe
Gellert Peter Kovacs <kovacs.g.peter@gmail.com>
2024-12-08 01:01:11 +0100

    animalfood many to many added

--- KK12FG_HSZF_2024251.Persistence.MsSql/PetRegistrationDbContext.cs
+++ KK12FG_HSZF_2024251.Persistence.MsSql/PetRegistrationDbContext.cs
@@ -16,6 +16,20 @@ namespace KK12FG_HSZF_2024251.Persistence.MsSql
         }
 
         public PetRegistrationDbContext(DbContextOptions<PetRegistrationDbContext> options) : base(options) { }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Animal>()
+                .HasMany(a => a.Foods)
+                .WithMany(f => f.Animals)
+                .UsingEntity<Dictionary<string, object>>(
+                    "AnimalFood", // Köztes tábla neve
+                    animal => animal.HasOne<Food>().WithMany().HasForeignKey("FoodId"),
+                    activity => activity.HasOne<Animal>().WithMany().HasForeignKey("AnimalId"));
+        
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=petregistrationdb;Integrated Security=True;MultipleActiveResultSets=true";
